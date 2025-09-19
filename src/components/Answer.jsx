d8bdddd4ae68e670b3b65a49ff81c637c1e04a95
若林悠斗 <ussr0101ss@gmail.com>
2025-09-19 11:18:28 +0900

    問題を解答した時にスコアを表示されるようにしました

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -9,11 +9,17 @@ const Answer = () => {
   const answer = location.state?.correctAnswer;
   const user_answer = location.state?.selected;
   const count = location.state?.count;
+  const currentScore = location.state?.score || 0;
+
+  // 正解ならスコアを1加算、不正解ならそのまま
+  const newScore = judge ? currentScore + 1 : currentScore;
+
 
   const nextquestion = () => {
     navigate("/question", {
       state: {
         count: count, // 現在の問題番号
+        score: newScore, // 更新したスコアを次のページに渡す
       },
     });
   };
@@ -23,6 +29,7 @@ const Answer = () => {
       <h1>あなたの回答は{judge ? "正解" : "不正解"}です</h1>
       <div> 正解は：{answer}</div>
       <div>あなたの回答は{user_answer}です</div>
+      <div>現在のスコアは{newScore}です</div>
       <button onClick={nextquestion}>次の問題へ</button>
     </div>
   );

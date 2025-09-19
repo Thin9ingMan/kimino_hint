73eee5402e0b800807bcd7e7c7c342ec3d5c70c5
Thin9ingMan <ceudhiesv253g7hsi43@gmail.com>
2025-09-19 14:48:50 +0900

    調整しました１

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,19 +1,19 @@
 import { Link, useLocation } from "react-router-dom";
 const Result = () => {
-    const location = useLocation();
-    const score = location.state?.score || 0;
-    const count = location.state?.count || 0;
+  const location = useLocation();
+  const score = location.state?.score || 0;
+  const count = location.state?.count || 0;
+  console.log("スコアとカウント", score, count);
   return (
     <div>
-        <div id="result">あなたの正解率は{Math.round((score/count)*100)}%です</div>
-        <Link to="/profile">
-            <button>プロフィールへ進む</button>
-        </Link>
-
-
+      <div id="result">
+        あなたの点数は{score} / {count * 3}点です
+      </div>
+      <Link to="/profile">
+        <button>プロフィールへ進む</button>
+      </Link>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Result
+export default Result;

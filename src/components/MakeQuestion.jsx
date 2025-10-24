527f33a8503ee37ed5a2ecd6e199522bd77a0b01
Thin9ingMan <ceudhiesv253g7hsi43@gmail.com>
2025-10-24 15:51:09 +0900

    makequestionの修正

--- src/components/MakeQuestion.jsx
+++ src/components/MakeQuestion.jsx
@@ -1,89 +1,90 @@
-import React from 'react'
-import { useState } from 'react'
-const MakeQuestion = () => {
-    const [ answer1 , setAnswer1 ] = useState("")
-    const [ answer2 , setAnswer2 ] = useState("")
-    const [ answer3 , setAnswer3 ] = useState("")
-    const [ answer4 , setAnswer4 ] = useState("")
-    const [ answer5 , setAnswer5 ] = useState("")
-    const [ answer6 , setAnswer6 ] = useState("")
+import React from "react";
+import { Link } from "react-router-dom";
 
-    const handleSubmit = (e) => {
+import { useState } from "react";
+const MakeQuestion = () => {
+  const [answer1, setAnswer1] = useState("");
+  const [answer2, setAnswer2] = useState("");
+  const [answer3, setAnswer3] = useState("");
+  const [answer4, setAnswer4] = useState("");
+  const [answer5, setAnswer5] = useState("");
+  const [answer6, setAnswer6] = useState("");
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    window.localStorage.setItem("answer1",answer1);
-    window.localStorage.setItem("answer2",answer2);
-    window.localStorage.setItem("answer3",answer3);
-    window.localStorage.setItem("answer4",answer4);
-    window.localStorage.setItem("answer5",answer5);
-    window.localStorage.setItem("answer6",answer6);
+    const answers = [answer1, answer2, answer3, answer4, answer5, answer6];
+    window.localStorage.setItem("answers", answers);
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <>
+      <form onSubmit={handleSubmit}>
         <label>
-            名前
-            <input 
+          名前
+          <input
             type="text"
-              value={answer1}
-              onChange={(e) => setAnswer1(e.target.value)}
-              placeholder="名前"
-              autoFocus>
-            </input>
-        </label> 
+            value={answer1}
+            onChange={(e) => setAnswer1(e.target.value)}
+            placeholder="名前"
+            autoFocus
+          ></input>
+        </label>
         <label>
-            学部
-            <input 
+          学部
+          <input
             type="text"
-              value={answer2}
-              onChange={(e) => setAnswer2(e.target.value)}
-              placeholder="学部"
-              autoFocus>
-            </input>
-        </label> 
+            value={answer2}
+            onChange={(e) => setAnswer2(e.target.value)}
+            placeholder="学部"
+            autoFocus
+          ></input>
+        </label>
         <label>
-            学年
-            <input 
+          学年
+          <input
             type="text"
-              value={answer3}
-              onChange={(e) => setAnswer3(e.target.value)}
-              placeholder="学年"
-              autoFocus>
-            </input>
-        </label> 
+            value={answer3}
+            onChange={(e) => setAnswer3(e.target.value)}
+            placeholder="学年"
+            autoFocus
+          ></input>
+        </label>
         <label>
-            趣味
-            <input 
+          趣味
+          <input
             type="text"
-              value={answer4}
-              onChange={(e) => setAnswer4(e.target.value)}
-              placeholder="趣味"
-              autoFocus>
-            </input>
-        </label> 
+            value={answer4}
+            onChange={(e) => setAnswer4(e.target.value)}
+            placeholder="趣味"
+            autoFocus
+          ></input>
+        </label>
         <label>
-            名前
-            <input 
+          名前
+          <input
             type="text"
-              value={answer5}
-              onChange={(e) => setAnswer5(e.target.value)}
-              placeholder="名前"
-              autoFocus>
-            </input>
-        </label> 
+            value={answer5}
+            onChange={(e) => setAnswer5(e.target.value)}
+            placeholder="名前"
+            autoFocus
+          ></input>
+        </label>
         <label>
-            好きなアーティスト
-            <input 
+          好きなアーティスト
+          <input
             type="text"
-              value={answer6}
-              onChange={(e) => setAnswer6(e.target.value)}
-              placeholder="好きなアーティスト"
-              autoFocus>
-            </input>
-        </label> 
-       <button type="submit">作成</button>
-        
-    </form>
-    
-  )
-}
+            value={answer6}
+            onChange={(e) => setAnswer6(e.target.value)}
+            placeholder="好きなアーティスト"
+            autoFocus
+          ></input>
+        </label>
+        <button type="submit">作成</button>
+      </form>
+      <Link to="/">
+        <button>戻る</button>
+      </Link>
+    </>
+  );
+};
 
-export default MakeQuestion
+export default MakeQuestion;

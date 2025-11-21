14f44112cf30cc77a278c9e8ce113b5256be8ad0
Thin9ingMan <ceudhiesv253g7hsi43@gmail.com>
2025-11-21 17:03:21 +0900

    自分のプロフィールのスタイルの修正

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -8,21 +8,21 @@ const MyProfile = () => {
     <div className="pf-wrap">
       <div className="pf-card">
         <h1 className="pf-title">プロフィール</h1>
-<ProfileCard
-                        profile={{
-                            name: answers.username,
-                            faculty: answers.department,
-                            hobby: answers.hobby,
-                            favoriteArtist: answers.artist,
-                        }}
-                        />
+        <ProfileCard
+          profile={{
+            name: answers.username,
+            faculty: answers.department,
+            hobby: answers.hobby,
+            favoriteArtist: answers.artist,
+          }}
+        />
 
         <div className="pf-actions">
-          <Link to="/">
-            <button className="pf-btn pf-btn-ghost">戻る</button>
-          </Link>
           <Link to="/make_question">
             <button className="pf-btn pf-btn-ghost">編集</button>
+          </Link>
+          <Link to="/">
+            <button className="pf-btn pf-btn-ghost">戻る</button>
           </Link>
         </div>
       </div>

c2e7372e07d54e83c43494ba1af42dcd44b526e8
きち <y01waiwai01y@gmail.com>
2025-11-21 16:01:36 +0900

    自分のプロフィールに関するCSSの変更（他人のプロフィールページと統一）、プロフィールページの不要なホバーを削除

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,35 +1,22 @@
 import { Link } from "react-router-dom";
 import "./Profile.css";
+import { ProfileCard } from "./ui/ProfileCard";
 
 const Profile = () => {
-  const answers = JSON.parse(localStorage.getItem("answers"));
+  const answers = JSON.parse(localStorage.getItem("answers"))
   return (
     <div className="pf-wrap">
       <div className="pf-card">
         <h1 className="pf-title">プロフィール</h1>
 
-        <dl className="pf-table">
-          <div className="pf-row">
-            <dt className="pf-label">名前</dt>
-            <dd className="pf-value">{answers["username"]}</dd>
-          </div>
-          <div className="pf-row">
-            <dt className="pf-label">学部</dt>
-            <dd className="pf-value">{answers["department"]}</dd>
-          </div>
-          <div className="pf-row">
-            <dt className="pf-label">学年</dt>
-            <dd className="pf-value">{answers["grade"]}</dd>
-          </div>
-          <div className="pf-row">
-            <dt className="pf-label">趣味</dt>
-            <dd className="pf-value">{answers["hobby"]}</dd>
-          </div>
-          <div className="pf-row">
-            <dt className="pf-label">好きなアーティスト</dt>
-            <dd className="pf-value">{answers["artist"]}</dd>
-          </div>
-        </dl>
+       <ProfileCard
+                        profile={{
+                            name: answers.username,
+                            faculty: answers.department,
+                            hobby: answers.hobby,
+                            favoriteArtist: answers.artist,
+                        }}
+                        />
 
         <div className="pf-actions">
           <Link to="/">
